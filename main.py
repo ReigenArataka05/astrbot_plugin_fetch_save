c7dce75ef207e141cff7cebd3992882e42aa66e4
凌夷の <74313654+ReigenArataka05@users.noreply.github.com>
2026-03-20 21:04:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class MyPlugin(Star):
 
 @filter.command("select")
 async def select(self, event: AstrMessageEvent, username: str): 
-    yield event.plain_result(f"开始查询用户 {username},请耐心等待...")
+    yield event.plain_result(f"查询 {username} 中，请耐心等待...")
     logger.info(f"正在查询用户: {username}")
     uid = await fetch_uid(username)
 
@@ -32,7 +32,7 @@ async def select(self, event: AstrMessageEvent, username: str):
 
     result = '✅ 查询成功！\n'
     result += f"用户名：{username} UID: {uid}\n"
-    result += f'{"索引":<6}\t{"昵称":<20}\t{"创建时间":<20}  {"更新时间":<20}  {"数据大小":<10}\n'
+    result += f'{"索引"}\t{"昵称":<16}\t{"创建时间":<20}  {"更新时间":<20} {"更新次数":<6}  {"数据大小":<10}\n'
 
     global selected_uid
     selected_uid = uid
@@ -42,7 +42,7 @@ async def select(self, event: AstrMessageEvent, username: str):
             save_data = await fetch_save_data(i, uid, GAMEID, GAMEKEY)
             # 添加数据验证
             if not save_data:
-                result += f'存档 {i}: 数据为空\n'
+                result += f'存档 {i+1}: 数据为空\n'
                 continue
                 
             data = json.loads(save_data)
@@ -55,13 +55,31 @@ async def select(self, event: AstrMessageEvent, username: str):
             datetime = data.get('datetime', '') #更新时间
             content = decrypt_save_data(content)
 
-            result += f'{i:<6}\t{title:<20}\t{create_time:<20}  {datetime:<20}  {update_times:<6}  {round(len(content)/1024/1024,2)}MB\n'
-
+            result += f'{i+1:<6}\t{title:<16}\t{create_time:<20}  {datetime:<20}  {update_times:<6}  {round(len(content)/1024/1024,2)}MB\n'
 
         except Exception as e:
-            result += f'{i:<6}\t{"空存档":<20}\n'
+            result += f'{i+1:<6}\t{"空存档":<16}\n'
             continue
     
     result+='当前UID已被记录，可对其进行<下载存档>与<分析异常>操作。'
     yield event.plain_result(result)
 
+@filter.command("download")
+async def select(self, event: AstrMessageEvent, index: int): 
+
+    if not selected_uid:
+        yield event.plain_result('请先使用select或selectuid命令查询存档！')
+    if index<=0 or index>8:
+        yield event.plain_result('索引错误！请输入1-8之间的数字。')
+
+    save_data = await fetch_save_data(index, selected_uid, GAMEID, GAMEKEY)
+    data = json.loads(save_data)
+    content = decrypt_save_data(data['data'])
+    yield event.plain_result(f'正在下载存档 {index}，请耐心等待...')
+
+    # 保存为文件并发送
+    file_path = f"{select_uid}_{index}.xml"
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+    yield event.send(file_path)
